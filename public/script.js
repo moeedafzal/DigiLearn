edb2cd77ef29224ac5952fbce8aba67d3894c9d5
Moeed Afzal <moeedafzal97@gmail.com>
2023-09-10 15:23:35 +0500

    added loading

--- public/script.js
+++ public/script.js
@@ -1,20 +1,35 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const loadingScreen = createLoadingScreen();
+  document.body.appendChild(loadingScreen);
+
   async function fetchModules() {
-    await fetch("/modules?moduleName=Unit one")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json(); // Parse the response as JSON
-      })
-      .then((data) => {
-        const testContainer = document.getElementById("test-container");
-        testContainer.innerHTML=JSON.stringify(data);
-      })
-      .catch((error) => {
-        console.error("There was a problem fetching the modules:", error);
-      });
+    try {
+      const response = await fetch("/modules?moduleName=Unit one");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const data = await response.json();
+
+      const testContainer = document.getElementById("test-container");
+      testContainer.innerHTML = JSON.stringify(data);
+
+    } catch (error) {
+      console.error("There was a problem fetching the modules:", error);
+    } finally {
+      document.body.removeChild(loadingScreen);
+    }
   }
 
   fetchModules();
 });
+
+
+function createLoadingScreen () {
+  const loadingScreen = document.createElement("div");
+  loadingScreen.className = "loading-screen";
+  loadingScreen.innerHTML = '<div class="loader"></div>';
+  
+  return loadingScreen;
+}
