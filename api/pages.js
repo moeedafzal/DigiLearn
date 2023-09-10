8d96147fc1784c2697964fa909b34820203ba9fa
Moeed Afzal <moeedafzal97@gmail.com>
2023-09-10 16:17:14 +0500

    added module cards

--- api/pages.js
+++ api/pages.js
@@ -9,27 +9,21 @@ const pool = new Pool({
   connectionString: process.env.POSTGRES_URL + "?sslmode=require",
 });
 
-router.get("/modules", async (req, res) => {
+router.get("/get-modules", async (req, res) => {
   try {
-    const moduleName = req.query.moduleName;
-    console.log("moduleName: ", moduleName);
-    if (!moduleName) {
-      throw new Error("Module name is required");
-    }
-    console.log("Module name: ", moduleName)
 
-    const query = `SELECT * FROM modules WHERE modules.name = '${moduleName}'`;
+    const query = `SELECT * FROM modules`;
 
     const client = await pool.connect();
-    const module = await client.query(query);
+    const modules = await client.query(query);
     client.release();
 
-    if (!module.rows.length) {
-      throw new Error("Module not found");
+    if (!modules.rows.length) {
+      throw new Error("No modules found");
     }
 
-    const row = module.rows[0];
-    return res.status(200).json({ row });
+    const rows = modules.rows
+    return res.status(200).json({ rows });
   } catch (error) {
     console.error("Error fetching pages:", error.message);
     res
--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   async function fetchModules() {
     try {
-      const response = await fetch("/modules?moduleName=Unit one");
+      const response = await fetch("/get-modules");
 
       if (!response.ok) {
         throw new Error("Network response was not ok");
@@ -12,9 +12,12 @@ document.addEventListener("DOMContentLoaded", function () {
 
       const data = await response.json();
 
-      const testContainer = document.getElementById("test-container");
-      testContainer.innerHTML = JSON.stringify(data);
+      const moduleCardsContainer = document.getElementById("module-cards");
 
+      data.rows.forEach((module) => {
+        const moduleCard = createModuleCard(module);
+        moduleCardsContainer.appendChild(moduleCard);
+      });
     } catch (error) {
       console.error("There was a problem fetching the modules:", error);
     } finally {
@@ -25,11 +28,35 @@ document.addEventListener("DOMContentLoaded", function () {
   fetchModules();
 });
 
-
-function createLoadingScreen () {
+function createLoadingScreen() {
   const loadingScreen = document.createElement("div");
   loadingScreen.className = "loading-screen";
   loadingScreen.innerHTML = '<div class="loader"></div>';
-  
+
   return loadingScreen;
+}
+
+function createModuleCard(module) {
+  const card = document.createElement("div");
+
+  // Create card content
+  const name = document.createElement("h2");
+  name.textContent = module.name;
+
+  const title = document.createElement("h3");
+  title.textContent = module.title;
+
+  const description = document.createElement("p");
+  description.textContent = module.description;
+
+  const seeModuleButton = document.createElement("button");
+  seeModuleButton.innerHTML = `<span>See Module</span> <img src="/img/vector.png" />`;
+
+  // Append content to card
+  card.appendChild(name);
+  card.appendChild(title);
+  card.appendChild(description);
+  card.appendChild(seeModuleButton);
+
+  return card;
 }
